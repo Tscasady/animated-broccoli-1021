a1bbf543b9761a5453c747ba62d067e8ed535463
Thomas Casady <tscasady@gmail.com>
2023-01-13 11:00:09 -0600

    feat: Add ability to remove a patient from a doctors care.

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,5 +1,18 @@
 class DoctorsController < ApplicationController
+  before_action :find_doctor, only: [:show, :update]
+  
   def show
+  end
+
+  def update
+    @doctor_patient = DoctorPatient.find_by(doctor: params[:id], patient: params[:patient_id])
+    @doctor_patient.destroy
+    redirect_to doctor_path
+  end
+
+  private
+
+  def find_doctor
     @doctor = Doctor.find(params[:id])
   end
 end
